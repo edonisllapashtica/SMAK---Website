ba0d116ea2c9125546d7f2aa9e5d24bc70a5275d
Edonis <62097212+edonisllapashtica@users.noreply.github.com>
2022-05-31 16:27:56 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ toggler.addEventListener("click", function () {
 
 let date = new Date().getFullYear();
 document.getElementById("date-with-js").innerText = date;
+
+// Scroll up icon
+let scrollUp = document.getElementById("scroll-up");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    scrollUp.style.transform = "translateY(0px)";
+  } else {
+    scrollUp.style.transform = "translateY(100px)";
+  }
+}
+
+// Loader
+let loader = document.getElementById('loader');
+
+window.onload = function() {
+  loader.classList.add('active')
+}
